de6a4255348eff8d2ce42d270259c4711afb8973
nazianah <nazianah.umme@gmail.com>
2023-11-12 00:29:14 +0400

    Add solutions for exercises 1-4 in script.js

---
+++ week2/Day2/exerciseXp/script.js
@@ -0,0 +1,38 @@
+//exercise#1
+// let x = 5;
+// let y = 2;
+// if (x > y) {
+//     console.log("x is the biggest number");
+// } else {
+//     console.log("y is the biggest number");
+// }
+
+//exercise#2    
+// let newDog = "Chihuahua";
+// console.log(newDog.length);
+// console.log(newDog.toUpperCase());
+// console.log(newDog.toLowerCase());
+// if (newDog === "Chihuahua") {
+//     console.log("I love Chihuahua, it’s my favorite dog breed");
+// } else {
+//     console.log("I dont care, I prefer cats");
+// }   
+//exercise#3
+// let userNumber = parseInt(prompt("Put a number"));
+// if (userNumber % 2 === 0) {
+//     alert(`${userNumber} is an even number`);
+// } else {
+//     alert(`${userNumber} is an odd number`);
+// }
+//exercise#4
+// const users = ["Lea123", "Princess45", "cat&doglovers", "helooo@000"];
+// console.log(users.length);
+// if (users.length === 0) {
+//     console.log("no one is online");
+// } else if (users.length === 1) {
+//     console.log(`${users[0]} is online`);
+// } else if (users.length === 2) {
+//     console.log(`${users[0]} and ${users[1]} are online`);
+// } else {
+//     console.log(`${users[0]}, ${users[1]} and ${users.length - 2} more are online`);
+// }

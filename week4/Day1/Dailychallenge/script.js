e28c67621dacbdfddc48edda5a4c8c7e31a12b4b
nazianah <nazianah.umme@gmail.com>
2023-11-20 22:47:24 +0400

    Fix bug in login functionality

---
+++ week4/Day1/Dailychallenge/script.js
@@ -0,0 +1,44 @@
+// const gameInfo = [
+//     {
+//       username: "john",
+//       team: "red",
+//       score: 5,
+//       items: ["ball", "book", "pen"]
+//     },
+//     {
+//       username: "becky",
+//       team: "blue",
+//       score: 10,
+//       items: ["tape", "backpack", "pen"]
+//     },
+//     {
+//       username: "susy",
+//       team: "red",
+//       score: 55,
+//       items: ["ball", "eraser", "pen"]
+//     },
+//     {
+//       username: "tyson",
+//       team: "green",
+//       score: 1,
+//       items: ["book", "pen"]
+//     },
+//    ];
+
+//    const username = []; 
+//    gameInfo.forEach((player) => {
+//     username.push(player.username + "!")
+//    });
+//     console.log(username);
+//     const winners = [];
+//     gameInfo.forEach((player) => {
+//         if(player.score > 5){
+//             winners.push(player.username)
+//         }
+//     });
+//     console.log(winners);
+//     let total = 0;
+//     gameInfo.forEach((player) => {
+//         total += player.score
+//     });
+//     console.log("totalscore: " + total);
